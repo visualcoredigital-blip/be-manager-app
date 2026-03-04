a73a42d1ded452d936a8d7ed173f9bf4e5da29ac
fchacon <fchacon@bnamericas.com>
2026-03-03 23:31:11 -0300

    configuración security

--- src/main/java/com/manager/app/config/SecurityConfig.java
+++ src/main/java/com/manager/app/config/SecurityConfig.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity; // <--- FALTA ESTA
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;    // <--- FALTA ESTA
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
