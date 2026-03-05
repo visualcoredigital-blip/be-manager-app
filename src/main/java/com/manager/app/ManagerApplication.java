3810e669c3059aeb7e354039cf58b69d825e0e2f
fchacon <fchacon@bnamericas.com>
2026-03-05 16:40:52 -0300

    configuration

--- src/main/java/com/manager/app/ManagerApplication.java
+++ src/main/java/com/manager/app/ManagerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@ComponentScan(basePackages = "com.manager.app") 
+@ComponentScan(basePackages = {"com.manager.app", "com.manager.app.config"}) // <--- FUERZA ESTO
 public class ManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ManagerApplication.class, args);
