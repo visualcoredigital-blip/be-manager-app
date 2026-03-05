62cb4a2510b475f93e003aaaff388c6395f1c79b
fchacon <fchacon@bnamericas.com>
2026-03-05 19:36:55 -0300

    cambios securityconfig

--- src/main/java/com/manager/app/config/RestTemplateConfig.java
+++ src/main/java/com/manager/app/config/RestTemplateConfig.java
@@ -3,6 +3,7 @@ package com.manager.app.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.http.client.SimpleClientHttpRequestFactory; 
 
 @Configuration
 public class RestTemplateConfig {
@@ -10,8 +11,8 @@ public class RestTemplateConfig {
     @Bean
     public RestTemplate restTemplate() {
         SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setConnectTimeout(3000); // 3 segundos para conectar
-        factory.setReadTimeout(3000);    // 3 segundos para esperar respuesta
+        factory.setConnectTimeout(5000);
+        factory.setReadTimeout(5000);
         return new RestTemplate(factory);
     }
 }
